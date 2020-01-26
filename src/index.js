6a9335b7f939c0f7afc3637d4a2476ea5cc2aa53
Victoria Hale <vic.hale74@gmail.com>
2020-01-26 15:41:15 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById("tasks")
+  //listen for submit event, and tel it what to do(callback function)
+  form.addEventListener('submit', function (event) { 
+    event.preventDefault()
+
+    //have user input
+    const newTask = document.getElementById('new-task-description').value 
+
+    //add it on the document
+    const taskItem = document.createElement("li")
+    taskItem.innerText = newTask
+    taskList.appendChild(taskItem)
+
+    //another way to append newTask, directly manipulates the DOM
+    //taskList.innerHTML += `<li> ${newTask} </li>`
+
+    //reset form description box after submit
+    form.reset()
+
+    })
+    
+    //event delegation
+    taskList.addEventListener("click", function(event) {
+      event.preventDefault
+    })
 });
+
+
+
+

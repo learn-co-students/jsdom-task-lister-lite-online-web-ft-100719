7a3a991c52de94e472d24febdd5acfd8dc8ec3fe
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-01-27 16:35:44 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", function(e) {
+  handleOnCreateSubmit()
+})
+function handleOnCreateSubmit() {
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let taskName = e.target['new-task-description'].value;
+    let tasksDiv = document.getElementById('tasks')
+    tasksDiv.innerHTML += `<li>${taskName}</li>`
+    form.reset()
+  })
+}

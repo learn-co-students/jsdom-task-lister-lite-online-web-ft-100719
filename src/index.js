1cd3d5b5ae4970fb1805ab0df61e67e2929dfb3c
lmjcbs <hello@liamjacobs.dev>
2020-02-04 10:39:24 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  handleFormOnSubmit()
 });
+
+const form = document.getElementById('create-task-form');
+
+function handleFormOnSubmit() {
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let taskName = e.target['new-task-description'].value;
+    let tasksDiv = document.getElementById('tasks')
+    tasksDiv.innerHTML += `<li>${taskName}</li>`
+    form.reset()
+  })
+}
